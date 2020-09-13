1e5cec7d3a8b96fea848ceb29ae7bd3e2e5809f8
Shad-Sheikh <66586570+Shad-Sheikh@users.noreply.github.com>
2020-09-13 13:47:01 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-void main()
+//Swaping of two number using third variable in C    => Document section
+#include <stdio.h>                                   //Link Section
+int main()                                           //main function with return type int
 {
-int num1, num2 ,tem;                   //Variables declaration
+int num1, num2 ,tem;                                 //Variables declaration
 printf("Enter two numbers : ");         
-scanf("%d%d", &num1, &num2);           //taking input
+scanf("%d%d", &num1, &num2);                         //taking input
 printf("Before Swapping  : \nFirst number = %d\nSecond number = %d\n",num1 ,num2);
 //swaping
 tem = num1;         
 num1 = num2;
 num2 = tem;
 printf("\nAfter Swapping : \nFirst numbers = %d\nSecond number = %d\n",num1 ,num2);
-}
+return 0;                                             //return 0 after complinting the task
