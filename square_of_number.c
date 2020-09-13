ccbc30173c7f46d277d5fd75a51115b057ed2a27
Shad-Sheikh <66586570+Shad-Sheikh@users.noreply.github.com>
2020-09-13 13:49:01 +0530

    Update square_of_number.c

--- square_of_number.c
+++ square_of_number.c
@@ -1,10 +1,11 @@
-//Program for calculating a square of a whole number
-#include<stdio.h>
-void main()
+//Program for calculating a square of a whole number in C    => Document section
+#include <stdio.h>                                           //Link Section
+int main()                                                   //main function with return type int
 {
-int num,sqr;                            //varible declaration
+int num,sqr;                                                  //varible declaration
 printf("Enter a number for Square : "); 
-scanf("%d",&num);                       //input a number
-sqr = num*num;                          //calculating square of a number
-printf("Square = %d",sqr);              // printing square
+scanf("%d",&num);                                             //input a number
+sqr = num*num;                                                //calculating square of a number
+printf("Square = %d",sqr);                                    // printing square
+return 0;                                                     //return 0 after complinting the task
 }
