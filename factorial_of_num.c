30d6ffca294becd04b0a54cb977f37ade6e6be68
Shad-Sheikh <66586570+Shad-Sheikh@users.noreply.github.com>
2020-09-13 17:52:59 +0530

    Create factorial_of_num.c

---
+++ factorial_of_num.c
@@ -0,0 +1,14 @@
+//Program for calculating a square of a whole number in C    => Document section
+#include <stdio.h>                                           //Link Section
+int main()                                                   //main function with return type int
+{
+int num , i , fact = 1;
+printf("Enter a number : ");
+scanf("%d",&num);                                          //taking input
+    for(i=1 ; i <= num ; i++)                               //for loop
+        {
+        fact = fact * i;                                    //calculating factorial
+        }
+    printf("Factorial of %d is %d",num,fact);               //printing factorial  
+return 0;                                                     //return 0 after complinting the task
+}
