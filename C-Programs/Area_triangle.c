7d970ca5d775cd01283c4ce97089a515affe1f98
Shad-Sheikh <66586570+Shad-Sheikh@users.noreply.github.com>
2020-10-18 22:21:02 +0530

    Create Area_triangle.c

---
+++ C-Programs/Area_triangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+float area,height,base;						 
+printf("Enter  value of Base and Highte of Triangle : ");
+scanf("%f%f",&base,&height);
+//Area of Trianel 1/2 x base x height
+area =  0.5 * base * height;
+printf("Area of Triangel is = %.2f",area);						 
+}
