b7e8107bd9c758055cb0b0025b55f4d7f0f67142
Shad-Sheikh <66586570+Shad-Sheikh@users.noreply.github.com>
2020-10-22 13:11:37 +0530

    Create 47.  Armstrong2.c

---
+++ C-Programs/47.  Armstrong2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int num,i,term,s=0,arm,tem,rem;
+printf("Enter a no:");
+scanf("%d",&num);
+tem=num;
+for(;1<=num;)
+    {
+    rem=num%10;
+    term=rem*rem*rem;
+    num=num/10;
+    arm = arm+term;
+    }
+if (arm==tem)
+    printf("%d is Armstrong number",tem);
+else
+    printf("%d is not Armstrong number",tem);
+}
