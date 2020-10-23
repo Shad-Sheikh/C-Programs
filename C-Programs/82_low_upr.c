ae5ff93265b9e668168774412b0950eb6f63bb3f
Shad-Sheikh <66586570+Shad-Sheikh@users.noreply.github.com>
2020-10-23 22:40:01 +0530

    Update 82_low_upr.c

--- C-Programs/82_low_upr.c
+++ C-Programs/82_low_upr.c
@@ -11,18 +11,3 @@ return 0;
 }
 */
 
-//3. WAP to accept two string and merge that string into third
-/*
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-char string1[30],string2[30],string[30];
-printf("Enter 1st string : ");
-gets(string1);
-printf("Enter 2nd string : ");
-gets(string2);
-strcpy(string,string1);
-printf("Marge string:",strcat(string,string2));
-puts(string);
-}
