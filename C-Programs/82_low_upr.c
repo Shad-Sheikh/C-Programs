b9349d107f2704c9c8e8a7b9408f6dee2366345c
Shad-Sheikh <66586570+Shad-Sheikh@users.noreply.github.com>
2020-10-23 22:39:26 +0530

    Create 82_low_upr.c

---
+++ C-Programs/82_low_upr.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char string[30];
+printf("Enter String : ");
+gets(string);
+printf("Upper case of string : ");
+puts(strupr(string));
+return 0;
+}
+*/
+
+//3. WAP to accept two string and merge that string into third
+/*
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char string1[30],string2[30],string[30];
+printf("Enter 1st string : ");
+gets(string1);
+printf("Enter 2nd string : ");
+gets(string2);
+strcpy(string,string1);
+printf("Marge string:",strcat(string,string2));
+puts(string);
+}
